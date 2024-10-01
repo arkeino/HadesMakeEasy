77cab7e0dfdc496738472a740b27705c0b491054
Jabir H <72025330+arkeino@users.noreply.github.com>
2024-10-01 02:57:27 +0200

    [ADD] My file with 100% succes on steam
    MODIF all file and change the stat to make the game easy !

---
+++ AchievementData.lua
@@ -0,0 +1,435 @@
+﻿GameData.AchievementData =
+{
+	DefaultAchievement =
+	{
+		DebugOnly = true,
+	},
+
+	AchClearTartarus =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchClearAsphodel =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchClearElysium =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchClearElysiumEM =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchClearHeatGate =
+	{
+		InheritFrom = { "DefaultAchievement" },
+	},
+
+	AchClearAnyRun =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			RequiresRunCleared = true,
+		},
+	},
+
+	AchClearRun4thAspect =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchBeatCharon =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchBronzeSkellyTrophy =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchSilverSkellyTrophy =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchFoundKeepsakes =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchLeveledKeepsakes =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchFoundSummon =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			RequiredAnyAssistKeepsake = true,
+		},
+	},
+
+	AchFoundAllSummons =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchMaxedAnyAspect =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	ActUnlockedAllAspects =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+
+	},
+
+	AchPickedManyBoons =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchPurgedLegendBoon =
+	{
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchPickedManyHammers =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchUnlockedAllWeapons =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			true,
+		},	
+	},
+
+	AchForgedBond =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchOlympiansCodex =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchManyBrokerTrades =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			true,
+		}
+	},
+
+	AchManyCosmetics =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchManyQuests =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchFishFromEachBiome =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchOrpheusSingsAgain =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchMirrorAllUnlocked =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchNyxChaosReunion =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchMeetChthonicGods =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchAresKills =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchSisyphusLiberation =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			true,{ "SisyphusLiberation", },
+		},
+	},
+
+	AchSingersReunion =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchMyrmidonReunion =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchMirrorUpgradeClears =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchPactUpgradesClears =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchWeaponClears =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchEliteAttributeKills =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchAccessAdminChamber =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchReachedEnding =
+	{
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchReachedEpilogue =
+	{
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchWellShopItems =
+	{
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchBuffedButterfly =
+	{
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchBuffedPlume =
+	{
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchCrushedThanatos =
+	{
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchGreaterCall =
+	{
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchGreaterCallSpurned =
+	{
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchSkellyKills =
+	{
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+
+	AchCerberusPets =
+	{
+		InheritFrom = { "DefaultAchievement" },
+		CompleteGameStateRequirements =
+		{
+			true,
+		},
+	},
+}
